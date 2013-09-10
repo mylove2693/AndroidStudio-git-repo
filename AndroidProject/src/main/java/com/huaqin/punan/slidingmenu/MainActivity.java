c0e48a6babb3120fa7bea9f72e5ae85710f1776a
punan <punan2010@gmail.com>
2013-09-10 09:48:44 +0800

    add function

--- AndroidProject/src/main/java/com/huaqin/punan/slidingmenu/MainActivity.java
+++ AndroidProject/src/main/java/com/huaqin/punan/slidingmenu/MainActivity.java
@@ -3,13 +3,27 @@ package com.huaqin.punan.slidingmenu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private Button mybtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mybtn = (Button)findViewById(R.id.mybtn);
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Button Clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 
