559f8589d675974d7d580c94395a2ae1aaf54b8a
punan <punan2010@gmail.com>
2013-09-11 17:43:27 +0800

    add widget

--- TestAndroid/src/main/java/com/huaqin/punan/test/MainActivity.java
+++ TestAndroid/src/main/java/com/huaqin/punan/test/MainActivity.java
@@ -3,13 +3,18 @@ package com.huaqin.punan.test;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
+
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_main);
+
+        
     }
 
 
