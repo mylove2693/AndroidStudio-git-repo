d544b57425ecdda43421dbf52ec0b00cb9d6606e
punan <punan2010@gmail.com>
2013-09-12 16:48:02 +0800

    add function

--- TestAndroid/src/main/java/com/huaqin/punan/test/MainActivity.java
+++ TestAndroid/src/main/java/com/huaqin/punan/test/MainActivity.java
@@ -1,5 +1,9 @@
 package com.huaqin.punan.test;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -8,15 +12,30 @@ import android.widget.GridView;
 public class MainActivity extends Activity {
 
     private GridView mGridView;
+    private BroadcastReceiver mMusicStateReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_main);
 
-        
+        mMusicStateReceiver = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+            }
+        };
+
+        IntentFilter mIntentFilter = new IntentFilter("");
+        registerReceiver(mMusicStateReceiver,mIntentFilter);
+
     }
 
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mMusicStateReceiver);
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,5 +43,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
 }
