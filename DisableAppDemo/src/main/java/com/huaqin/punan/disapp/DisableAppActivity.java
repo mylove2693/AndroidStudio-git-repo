5261950aca19f4b32d4147679b297801ccff7979
punan <punan2010@gmail.com>
2013-09-24 21:05:45 +0800

    app disable demo

--- DisableAppDemo/src/main/java/com/huaqin/punan/disapp/DisableAppActivity.java
+++ DisableAppDemo/src/main/java/com/huaqin/punan/disapp/DisableAppActivity.java
@@ -1,15 +1,41 @@
 package com.huaqin.punan.disapp;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DisableAppActivity extends Activity {
+
+    private final static String TAG = "DisableAppMain";
+
+    private PackageManager mPackageManager;
+    private TextView mytv;
+    private Button mybtn;
+
+    private String mms = "com.android.mms";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.disable_app_activity);
+
+        mytv = (TextView)findViewById(R.id.mytv);
+        mybtn = (Button)findViewById(R.id.mybtn);
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPackageManager.setApplicationEnabledSetting(mms,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER,
+                        0);
+            }
+        });
+
+
+
     }
 
 
