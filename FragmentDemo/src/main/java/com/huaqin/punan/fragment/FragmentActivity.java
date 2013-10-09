f1146853eb4a8319f8c524218fa2a4a63dbdd235
punan <punan2010@gmail.com>
2013-10-09 15:58:24 +0800

    add Fragment demo

---
+++ FragmentDemo/src/main/java/com/huaqin/punan/fragment/FragmentActivity.java
@@ -0,0 +1,23 @@
+package com.huaqin.punan.fragment;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FragmentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.fragment, menu);
+        return true;
+    }
+    
+}
