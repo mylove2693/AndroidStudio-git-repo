ce49323c0f024f1d01dbed66be3b4e5c1408a030
punan <punan2010@gmail.com>
2013-10-10 09:03:39 +0800

    add Fragment demo

--- FragmentDemo/src/main/java/com/huaqin/punan/fragment/FragmentActivity.java
+++ FragmentDemo/src/main/java/com/huaqin/punan/fragment/FragmentActivity.java
@@ -10,7 +10,7 @@ public class FragmentActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        
+
     }
 
 
