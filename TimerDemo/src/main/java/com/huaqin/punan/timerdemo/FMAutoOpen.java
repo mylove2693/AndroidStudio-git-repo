4aa193bd713c41cdcca587a0ade28c498b44a9f7
punan <punan2010@gmail.com>
2013-11-26 15:20:26 +0800

    add function

---
+++ TimerDemo/src/main/java/com/huaqin/punan/timerdemo/FMAutoOpen.java
@@ -0,0 +1,25 @@
+package com.huaqin.punan.timerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by punan on 11/22/13.
+ */
+public class FMAutoOpen extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
